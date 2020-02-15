3a758c3794f83d3c00c3e3f9259adedf676c3547
Daniel Cruz <d.cruz.489@hotmail.com>
2020-02-14 21:53:58 -0700

    mvp

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -47,7 +47,7 @@ router.post('/login', (req, res) => {
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
         req.session.loggedin = true;
-        res.status(200).json({ message: `Welcome ${user.username}! Would you like a crumpet?`, });
+        res.status(200).json({ message: `Welcome ${user.username}! Help yourself to a cooke.`, });
       } else {
         res.status(401).json({ message: '...try try again.' });
       }
