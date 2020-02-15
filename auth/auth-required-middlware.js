8550a7469e9c41952d030255fabf9ce097938c47
Daniel Cruz <d.cruz.489@hotmail.com>
2020-02-14 21:40:34 -0700

    added session middleware and updated users model

--- auth/auth-required-middlware.js
+++ auth/auth-required-middlware.js
@@ -1,22 +1,31 @@
-const users = require('../users/users-model.js');
-const bcrypt = require('bcryptjs');
+// const users = require('../users/users-model.js');
+// const bcrypt = require('bcryptjs');
+
+// module.exports = (req, res, next) => {
+   
+//     const { username, password } = req.headers
+    
+//     if (!(username && password)) {
+//         res.status(401).json({ message: "You shall not pass!" });
+//     } else {
+//         users.findBy({ username })
+//             .first()
+//             .then(user => {
+//                 if (user && bcrypt.compareSync(password, user.password)) {
+//                     next()
+//                 } else {
+//                     res.status(401).json({ messege: "You shall not pass!" })
+//                 }
+//             })
+//             .catch((err) => { res.status(500).json({ messege: err }) })
+//     }
+// }
 
 module.exports = (req, res, next) => {
-   
-    const { username, password } = req.headers
-    
-    if (!(username && password)) {
-        res.status(401).json({ message: "You shall not pass!" });
-    } else {
-        users.findBy({ username })
-            .first()
-            .then(user => {
-                if (user && bcrypt.compareSync(password, user.password)) {
-                    next()
-                } else {
-                    res.status(401).json({ messege: "You shall not pass!" })
-                }
-            })
-            .catch((err) => { res.status(500).json({ messege: err }) })
-    }
-}
+
+    if (req.session.loggedin && (req.session.loggedin === true)) {
+        next();
+      } else {
+        res.status(400).json({ message: "Where, pray tell, is your cookie?" });
+      }
+    };
--- users/users-model.js
+++ users/users-model.js
@@ -7,29 +7,47 @@ module.exports = {
   findById,
 };
 
+// function find() {
+//   return db('users').select('id', 'username');
+// }
+
+// function findBy(filter) {
+//   return db('users')
+//     .select('id', 'username', 'password')
+//     .where(filter);
+// }
+
+// function add(user) {
+//   return db('users')
+//     .insert(user, 'id')
+//     .then(ids => {
+//       const [id] = ids;
+//       return findById(id);
+//     });
+// }
+
+// function findById(id) {
+//   return db('users')
+//     .select('id', 'username')
+//     .where({ id })
+//     .first();
+// }
+
 function find() {
   return db('users').select('id', 'username');
 }
 
 function findBy(filter) {
-  console.log(filter);
-  return db('users')
-    .select('id', 'username', 'password')
-    .where(filter);
+  return db('users').where(filter);
 }
 
-function add(user) {
-  return db('users')
-    .insert(user, 'id')
-    .then(ids => {
-      const [id] = ids;
-      return findById(id);
-    });
+async function add(user) {
+  const [id] = await db('users').insert(user);
+  return findById(id);
 }
 
 function findById(id) {
   return db('users')
-    .select('id', 'username')
     .where({ id })
     .first();
 }
